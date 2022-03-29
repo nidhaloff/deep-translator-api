d858fda4df4e3a99839a7b9dc843ed0f32d4052a
nidhal baccouri <nidhalbacc@gmail.com>
2022-03-29 19:41:40 +0200

    disabled pycache

--- src/models/responses.py
+++ src/models/responses.py
@@ -3,5 +3,7 @@ from pydantic import BaseModel
 
 
 class TranslationResponse(BaseModel):
+    """class that represents the response api."""
+
     translation: Optional[str] = None
     error: Optional[str] = None
