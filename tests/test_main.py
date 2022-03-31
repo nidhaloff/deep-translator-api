6f6c082ba3d1e8770440809c21ce8392a6c44de7
nidhal baccouri <nidhalbacc@gmail.com>
2022-03-31 23:09:41 +0200

    updated workflows

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,3 @@
-
-
-
 from src.main import app
 from fastapi.testclient import TestClient
 from tests.data import j, j_with_api
@@ -13,30 +10,41 @@ client = TestClient(app)
 
 
 def get_translator_resp(translator: str, json: str = j):
-    resp = client.post(f'/{translator}/', json=json)
+    resp = client.post(f"/{translator}/", json=json)
     return resp.status_code
 
 
 def test_google():
-    assert get_translator_resp('google') == SUCCESS_CODE
+    assert get_translator_resp("google") == SUCCESS_CODE
+
 
 def test_mymemory():
-    assert get_translator_resp('mymemory') == SUCCESS_CODE
+    assert get_translator_resp("mymemory") == SUCCESS_CODE
+
+
 def test_pons():
-    assert get_translator_resp('pons') == SUCCESS_CODE
+    assert get_translator_resp("pons") == SUCCESS_CODE
+
+
 def test_linguee():
-    assert get_translator_resp('linguee') == SUCCESS_CODE
+    assert get_translator_resp("linguee") == SUCCESS_CODE
+
+
 def test_qcri():
-    assert get_translator_resp('qcri', j_with_api) == SUCCESS_CODE
+    assert get_translator_resp("qcri", j_with_api) == SUCCESS_CODE
+
 
 def test_microsoft():
-    assert get_translator_resp('microsoft', j_with_api) == SUCCESS_CODE
+    assert get_translator_resp("microsoft", j_with_api) == SUCCESS_CODE
+
 
 def test_deepl():
-    assert get_translator_resp('deepl', j_with_api) == SUCCESS_CODE
+    assert get_translator_resp("deepl", j_with_api) == SUCCESS_CODE
+
 
 def test_libre():
-    assert get_translator_resp('libre', j_with_api) == SUCCESS_CODE
+    assert get_translator_resp("libre", j_with_api) == SUCCESS_CODE
+
 
 def test_yandex():
-    assert get_translator_resp('yandex', j_with_api) == SUCCESS_CODE
+    assert get_translator_resp("yandex", j_with_api) == SUCCESS_CODE
